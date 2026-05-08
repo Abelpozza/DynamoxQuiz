fd2135e0d5279d3f86aa91e787b950ef4468ed60
abel <abelfloripa2000@gmail.com>
2026-05-08 11:53:21 -0300

    estilização de alternativas + options

--- app/src/main/java/com/abel/dynamoxquiz/presentation/quiz/QuizScreen.kt
+++ app/src/main/java/com/abel/dynamoxquiz/presentation/quiz/QuizScreen.kt
@@ -10,10 +10,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 @Composable
 fun QuizScreen(
     viewModel: QuizViewModel
@@ -53,10 +65,60 @@ fun QuizScreen(
             uiState.question != null -> {
 
                 val question = uiState.question
+                var selectedOption by remember {
+                    mutableStateOf<String?>(null)
+                }
 
-                Text(
-                    text = question?.statement ?: ""
-                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Text(
+                        text = question?.statement ?: "",
+                        style = MaterialTheme
+                            .typography
+                            .headlineSmall
+                    )
+                    Spacer(modifier = Modifier.padding(8.dp))
+
+                    question?.options?.forEachIndexed { index, option ->
+
+                        val optionLetter = ('A' + index)
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 12.dp)
+                                .border(
+                                    width = 1.dp,
+                                    color = Color.Gray,
+                                    shape = RoundedCornerShape(12.dp)
+                                )
+                                .clickable {
+                                    selectedOption = option
+                                }
+                                .padding(16.dp),
+
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = selectedOption == option,
+                                onClick = {
+                                    selectedOption = option
+                                }
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+
+                            Text(
+                                text = "$optionLetter) $option",
+
+                                style = MaterialTheme
+                                    .typography
+                                    .bodyLarge
+                            )
+                        }
+                    }
+                }
             }
         }
     }
