a8ec9a10e35e6523483444ae76caeb5f53f77398
abel <abelfloripa2000@gmail.com>
2026-05-10 03:45:05 -0300

    ajuste de função button + sistema de pontuação e reset de test

--- app/src/main/java/com/abel/dynamoxquiz/presentation/quiz/QuizScreen.kt
+++ app/src/main/java/com/abel/dynamoxquiz/presentation/quiz/QuizScreen.kt
@@ -1,5 +1,6 @@
 package com.abel.dynamoxquiz.presentation.quiz
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,9 +9,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -26,8 +30,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.abel.dynamoxquiz.R
 
 @Composable
 fun QuizScreen(
@@ -35,145 +43,287 @@ fun QuizScreen(
 ) {
 
     val uiState by viewModel.uiState.collectAsState()
-
     val isCorrectFromApi by
     viewModel.isCorrect.collectAsState()
+    val currentQuestion by
+    viewModel.currentQuestion.collectAsState()
+    val score by
+    viewModel.score.collectAsState()
+    val quizFinished by
+    viewModel.quizFinished.collectAsState()
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(
+                rememberScrollState()
+            )
             .padding(24.dp),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement =
+            Arrangement.Center,
         horizontalAlignment =
             Alignment.CenterHorizontally
     ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 24.dp),
+            horizontalArrangement =
+                Arrangement.Start
+        ) {
+
+            Image(
+                painter = painterResource(
+                    id = R.drawable.dynamox_logo
+                ),
+                contentDescription =
+                    "Logo Dynamox",
+                modifier = Modifier
+                    .width(90.dp),
+                contentScale =
+                    ContentScale.Fit
+            )
+        }
+
         when {
             uiState.isLoading -> {
                 CircularProgressIndicator()
             }
             uiState.error != null -> {
                 Text(
-                    text = uiState.error ?: "Unknown error",
+                    text =
+                        uiState.error
+                            ?: "Unknown error",
                     color = MaterialTheme
                         .colorScheme
                         .error
                 )
             }
             uiState.question != null -> {
-
-                val question = uiState.question
-                var selectedOption by remember {
-                    mutableStateOf<String?>(null)
-                }
-                var answerChecked by remember {
-                    mutableStateOf(false)
-                }
-                Column(
-                    horizontalAlignment =
-                        Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = question?.statement ?: "",
-                        style = MaterialTheme
-                            .typography
-                            .headlineSmall
-                    )
-                    Spacer(
-                        modifier = Modifier
-                            .padding(8.dp)
-                    )
-                    question?.options?.forEachIndexed { index, option ->
-                        val optionLetter =
-                            ('A' + index)
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 12.dp)
-                                .border(
-                                    width = 1.dp,
-                                    color = Color.Gray,
-                                    shape =
-                                        RoundedCornerShape(12.dp)
-                                )
-                                .clickable {
-                                    selectedOption = option
-                                }
-                                .padding(16.dp),
-
-                            verticalAlignment =
-                                Alignment.CenterVertically
-                        ) {
-                            RadioButton(
-                                selected =
-                                    selectedOption == option,
-                                onClick = {
-                                    selectedOption = option
-                                }
-                            )
-                            Spacer(
-                                modifier = Modifier
-                                    .width(8.dp)
-                            )
-                            Text(
-                                text =
-                                    "$optionLetter) $option",
-
-                                style = MaterialTheme
-                                    .typography
-                                    .bodyLarge,
-                                fontWeight =
-                                    FontWeight.Normal
-                            )
-                        }
-                    }
-                    Button(
-                        onClick = {
-                            if (!answerChecked) {
-                                selectedOption?.let {
-                                    viewModel.sendAnswer(it)
-                                    answerChecked = true
-                                }
-
-                            } else {
-                                selectedOption = null
-                                answerChecked = false
-                                viewModel.loadQuestion()
-                            }
-                        },
-                        colors =
-                            ButtonDefaults.buttonColors(
-                                containerColor =
-                                    Color.Black,
-                                contentColor =
-                                    Color.White
-                            ),
-                        modifier = Modifier
-                            .padding(top = 24.dp)
+                if (quizFinished) {
+                    Column(
+                        horizontalAlignment =
+                            Alignment.CenterHorizontally
                     ) {
                         Text(
-                            if (!answerChecked)
-                                "Verificar resposta"
-                            else
-                                "Próxima pergunta"
-                        )
-                    }
-                    if (answerChecked) {
-                        Text(
                             text =
-                                if (isCorrectFromApi == true)
-                                    "✅Resposta correta✅"
-                                else
-                                    "❌Resposta incorreta❌",
-                            color =
-                                if (isCorrectFromApi == true)
-                                    Color(0xFF2E7D32)
-                                else
-                                    Color.Red,
-                            modifier = Modifier
-                                .padding(top = 16.dp),
+                                "🎉 Quiz Finalizado!",
+                            style = MaterialTheme
+                                .typography
+                                .headlineMedium,
                             fontWeight =
                                 FontWeight.Bold
                         )
+                        Spacer(
+                            modifier = Modifier
+                                .height(24.dp)
+                        )
+
+                        Text(
+                            text =
+                                "Sua pontuação foi:",
+                            style = MaterialTheme
+                                .typography
+                                .titleLarge
+                        )
+                        Spacer(
+                            modifier = Modifier
+                                .height(12.dp)
+                        )
+                        Text(
+                            text = "$score / 10",
+                            style = MaterialTheme
+                                .typography
+                                .headlineLarge,
+                            color = Color.Black,
+                            fontWeight =
+                                FontWeight.Bold
+                        )
+                        Spacer(
+                            modifier = Modifier
+                                .height(32.dp)
+                        )
+
+                        Button(
+                            onClick = {
+                                viewModel.restartQuiz()
+                            },
+
+                            colors =
+                                ButtonDefaults.buttonColors(
+                                    containerColor =
+                                        Color.Black,
+                                    contentColor =
+                                        Color.White
+                                )
+
+                        ) {
+
+                            Text(
+                                "Reiniciar Quiz"
+                            )
+                        }
+                    }
+
+                } else {
+                    val question =
+                        uiState.question
+                    var selectedOption by remember {
+                        mutableStateOf<String?>(null)
+                    }
+                    var answerChecked by remember {
+                        mutableStateOf(false)
+                    }
+                    Column(
+                        horizontalAlignment =
+                            Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text =
+                                "Pergunta $currentQuestion de 10",
+
+                            fontSize = 20.sp,
+                            style = MaterialTheme
+                                .typography
+                                .titleMedium,
+
+                            color = Color.Black,
+                            fontWeight =
+                                FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(bottom = 20.dp)
+                        )
+                        Text(
+                            text =
+                                question?.statement ?: "",
+                            style = MaterialTheme
+                                .typography
+                                .headlineSmall
+                        )
+                        Spacer(
+                            modifier = Modifier
+                                .padding(8.dp)
+                        )
+                        question?.options?.forEachIndexed { index, option ->
+                            val optionLetter =
+                                ('A' + index)
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 12.dp)
+                                    .border(
+                                        width = 1.dp,
+                                        color = Color.Gray,
+                                        shape =
+                                            RoundedCornerShape(12.dp)
+                                    )
+                                    .clickable {
+                                        selectedOption = option
+                                    }
+                                    .padding(16.dp),
+
+                                verticalAlignment =
+                                    Alignment.CenterVertically
+                            ) {
+                                RadioButton(
+                                    selected =
+                                        selectedOption == option,
+                                    onClick = {
+                                        selectedOption = option
+                                    }
+                                )
+                                Spacer(
+                                    modifier = Modifier
+                                        .width(8.dp)
+                                )
+                                Text(
+                                    text =
+                                        "$optionLetter) $option",
+                                    style = MaterialTheme
+                                        .typography
+                                        .bodyLarge,
+                                    fontWeight =
+                                        FontWeight.Normal
+                                )
+                            }
+                        }
+
+                        Button(
+                            onClick = {
+                                if (!answerChecked) {
+
+                                    selectedOption?.let {
+
+                                        viewModel.sendAnswer(it)
+
+                                        answerChecked = true
+                                    }
+
+                                } else {
+
+                                    if (currentQuestion < 10) {
+
+                                        selectedOption = null
+
+                                        answerChecked = false
+
+                                        viewModel.loadQuestion()
+
+                                    } else {
+
+                                        viewModel.finishQuiz()
+                                    }
+                                }
+                            },
+                            shape =
+                                RoundedCornerShape(26.dp),
+                            colors =
+                                ButtonDefaults.buttonColors(
+                                    containerColor =
+                                        Color.Black,
+                                    contentColor =
+                                        Color.White
+                                ),
+
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(70.dp)
+                                .padding(top = 24.dp)
+                        ) {
+
+                            Text(
+                                if (!answerChecked) {
+
+                                    "Verificar resposta"
+                                } else {
+                                    if (currentQuestion == 10)
+                                        "Ver pontuação"
+                                    else
+                                        "Próxima pergunta"
+                                }
+                            )
+                        }
+
+                        if (answerChecked) {
+                            Text(
+                                text =
+                                    if (isCorrectFromApi == true)
+                                        "✅Resposta correta✅"
+                                    else
+                                        "❌Resposta incorreta❌",
+                                color =
+                                    if (isCorrectFromApi == true)
+                                        Color(0xFF2E7D32)
+                                    else
+                                        Color.Red,
+                                modifier = Modifier
+                                    .padding(top = 16.dp),
+
+                                fontWeight =
+                                    FontWeight.Bold
+                            )
+                        }
                     }
                 }
             }
--- app/src/main/java/com/abel/dynamoxquiz/presentation/quiz/QuizViewModel.kt
+++ app/src/main/java/com/abel/dynamoxquiz/presentation/quiz/QuizViewModel.kt
@@ -22,6 +22,18 @@ class QuizViewModel(
     private val _isCorrect =
         MutableStateFlow<Boolean?>(null)
 
+    private val _currentQuestion = MutableStateFlow(1)
+
+    val currentQuestion: StateFlow<Int> = _currentQuestion.asStateFlow()
+
+    private val _score = MutableStateFlow(0)
+
+    val score: StateFlow<Int> = _score.asStateFlow()
+
+    private val _quizFinished = MutableStateFlow(false)
+
+    val quizFinished: StateFlow<Boolean> = _quizFinished.asStateFlow()
+
     val isCorrect: StateFlow<Boolean?> =
         _isCorrect.asStateFlow()
 
@@ -55,23 +67,62 @@ class QuizViewModel(
     }
 
     fun sendAnswer(answer: String) {
+
         val questionId =
             _uiState.value.question?.id ?: return
+
         viewModelScope.launch {
+
             try {
+
                 val response =
                     repository.sendAnswer(
                         questionId = questionId,
                         answer = answer
                     )
+
                 println("RESPOSTA DA API -> $response")
+
                 _isCorrect.value =
                     response.result
+
+                if (response.result) {
+
+                    _score.value++
+                }
+
+                if (_currentQuestion.value >= 10) {
+
+                    _quizFinished.value = true
+
+                } else {
+
+                    _currentQuestion.value++
+                }
+
             } catch (exception: Exception) {
+
                 _uiState.value = QuizUiState(
                     error = exception.message
                 )
             }
         }
+    }
+
+    fun restartQuiz() {
+
+        _score.value = 0
+
+        _currentQuestion.value = 1
+
+        _quizFinished.value = false
+
+        _isCorrect.value = null
+
+        loadQuestion()
+    }
+    fun finishQuiz() {
+
+        _quizFinished.value = true
     }
 }
